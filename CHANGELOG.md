a04ea0fd2e328fbc01c6769af58e66c5093ca29a
Harsha Krishnareddy <hvkrishn@us.ibm.com>
2018-07-06 17:50:02 -0700

    v0.0.1.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,6 @@ Changelog
 
 All notable changes to this project will be documented in this file.
 
-## [0.0.1.3] - 2018-07-04
+## [0.0.1.6] - 2018-07-04
 
 * Initial commit
--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = ['requests==2.18.4', 'httmock==1.2.5', 'python-jose==1.4.0']
+
 setup(
     name='py-keycloak',
-    version='0.0.1.3',
+    version='0.0.1.6',
     url='https://github.com/c0mpiler/py-keycloak',
     license='Apache License 2.0',
     author='c0mpiler',
@@ -17,14 +19,17 @@ setup(
     long_description_content_type="text/markdown",
     description=u'py-keycloak is a Python package providing access to the Keycloak API.',
     packages=['keycloak', 'keycloak.authorization', 'keycloak.tests'],
-    install_requires=['requests==2.18.4', 'httmock==1.2.5', 'python-jose==1.4.0'],
+    install_requires=requirements,
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: Apache Software License',
         'Development Status :: 3 - Alpha',
-        'Operating System :: MacOS',
-        'Operating System :: Unix',
-        'Operating System :: Microsoft :: Windows',
-        'Topic :: Utilities'
-    ]
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+    ],
+    setup_requires=requirements,
+    test_suite='tests',
+    zip_safe=False,
 )
